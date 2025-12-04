53ab8f7c55a52901bf8cc644773496dacdb84fcc
BRGFUCKERY <borgore@buygore.com>
2025-12-04 16:44:54 -0700

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,35 +1,17 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
+#include <JuceHeader.h>
 
-#include "JuceHeader.h"
-#include "PluginProcessor.h"
+class FruityClipAudioProcessor;
 
-//==============================================================================
-/**
-*/
-class FRUITYCLIPAudioProcessorEditor  : public juce::AudioProcessorEditor
+class FruityClipAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    FRUITYCLIPAudioProcessorEditor (FRUITYCLIPAudioProcessor&);
-    ~FRUITYCLIPAudioProcessorEditor() override;
+    FruityClipAudioProcessorEditor (FruityClipAudioProcessor&);
+    ~FruityClipAudioProcessorEditor() override {}
 
-    //==============================================================================
     void paint (juce::Graphics&) override;
-    void resized() override;
+    void resized() override {}
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    FRUITYCLIPAudioProcessor& audioProcessor;
-
-    juce::Image backgroundImage;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FRUITYCLIPAudioProcessorEditor)
+    FruityClipAudioProcessor& processor;
 };
