18132b2ce310902ae3e300a64ab3113908548564
BRGFUCKERY <borgore@buygore.com>
2025-12-06 14:11:41 -0800

    Update JuceHeader.h

--- Source/JuceHeader.h
+++ Source/JuceHeader.h
@@ -13,5 +13,5 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 
-// NEW: for Oversampling
+// for Oversampling / DSP
 #include <juce_dsp/juce_dsp.h>
