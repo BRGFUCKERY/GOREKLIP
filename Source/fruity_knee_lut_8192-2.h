3a1914f21699dcb6682ddf2e802bc676ffc11938
BRGFUCKERY <247777275+BRGFUCKERY@users.noreply.github.com>
2025-12-18 00:50:09 +0200

    Update fruity_knee_lut_8192-2.h

--- Source/fruity_knee_lut_8192-2.h
+++ Source/fruity_knee_lut_8192-2.h
@@ -1050,8 +1050,8 @@ static inline float processSample (float x) noexcept
     const float sign = (x >= 0.0f ? 1.0f : -1.0f);
     const float ax   = std::fabs (x);
 
-    if (ax <= kKneeStart)
-        return x;
+   if (ax <= kKneeStart) return x * 0.999f;
+
 
     if (ax >= kKneeEnd)
         return sign * kKneeLut[(size_t) (kLutSize - 1)];
